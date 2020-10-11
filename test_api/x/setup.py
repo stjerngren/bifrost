e2de155d7d0bb180d04485a36a4ce24c2924ed88
Axel Stjerngren <2294692s@student.gla.ac.uk>
2020-10-11 12:52:18 +0200

    Removing all files in .gitignore

---
+++ test_api/x/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="x", 
+    version="0.0.1",
+    author="Axel Stjerngren",
+    author_email="axelstjerngren@protonmail.com",
+    description="A package for TVM and STONNE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/axelstjerngren/level-4-project",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
---
+++ test_api/x/x/stonne/config.py
@@ -0,0 +1,10 @@
+
+
+
+
+def build_config():
+     """
+     This function builds a STONNE config and returns a simualtion object
+     """
+
+     return None
