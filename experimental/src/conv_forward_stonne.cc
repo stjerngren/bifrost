64bf1f3bf878e56da369b4279894a3bd6b1cac6d
Axel Stjerngren <2294692s@student.gla.ac.uk>
2020-11-17 17:49:48 +0100

    Conv2d wors

--- experimental/src/conv_forward_stonne.cc
+++ experimental/src/conv_forward_stonne.cc
@@ -47,17 +47,21 @@ namespace tvm
                     std::cout << *(data + i) << ", ";
 
                 std::cout << "\n converted \n";
-                output->ndim = input->ndim;
-                output->shape = input->shape;
+               // output->ndim = input->ndim;
+                //output->shape = input->shape;
                 output->data = data;
-                output->byte_offset = input->byte_offset;
+                //output->ctx = input->ctx;
+                //output->dtype = input->dtype;
+                //output->strides = input->strides;
+                //output->byte_offset = input->byte_offset;
                 for ( int i = 0; i < 34; i++)
                     std::cout << *(static_cast<float*>(output->data) + i) << ", ";
-                std::cout << (output->shape)[1];
-                std::cout << (output->shape)[2];
-                std::cout << (output->shape)[3];
-                std::cout << (output->shape)[0];
+                //std::cout << (output->shape)[1];
+                //std::cout << (output->shape)[2];
+                //std::cout << (output->shape)[3];
+                //std::cout << (output->shape)[0];
 
+                *ret = data;
   
             });
 
@@ -90,15 +94,9 @@ namespace tvm
                     stonne_config.loadFile(path_to_arch_file);
                 }
 
-                float *input_raw = static_cast<float *>(input->data);
+                float *input_raw  = static_cast<float *>(input->data);
                 float *weight_raw = static_cast<float *>(weight->data);
-
-                float output_array[32];
-                for (int i = 0; i < 32; i++)
-                {
-                    output_array[i] = 0.0;
-                }
-                float *output_raw = output_array;
+                float *output_raw = static_cast<float *>(output->data);
 
                 std::string layer_name = "Conv2dLayer";
                 simulateDenseConvForward(
@@ -122,11 +120,9 @@ namespace tvm
                     path_to_tile,
                     stonne_config);
 
-                output->shape = input->shape;
-                output->byte_offset = input->byte_offset;
-                output->data = output_raw;
+  
                 for (int i = 0; i < 34; i++)
-                    std::cout << *(static_cast<float *>(output->data) + i) << ", ";
+                    std::cout << *(output_raw + i) << ", ";
             });
 
     } // namespace contrib
