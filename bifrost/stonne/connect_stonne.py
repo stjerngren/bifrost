9200bc556d3ce47a8dbdc26e794074e952257006
Axel Stjerngren <2294692s@student.gla.ac.uk>
2021-01-26 19:00:14 +0000

    Remove debugging print statements #18

--- bifrost/stonne/connect_stonne.py
+++ bifrost/stonne/connect_stonne.py
@@ -13,8 +13,6 @@ def load_lib():
     dirname = os.path.dirname(__file__)
     stonne_conv2d = os.path.join(dirname, "../lib/conv_forward.so")
 
-    print(dirname)
-    print(stonne_conv2d)
     # load in as global so the global extern symbol is visible to other dll.
     lib = ctypes.CDLL(stonne_conv2d, ctypes.RTLD_GLOBAL)
     return lib
