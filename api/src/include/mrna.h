15164eee9158ba430baaeb8d7f63a4427df4986a
Axel Stjerngren <2294692s@student.gla.ac.uk>
2021-08-02 21:38:06 +0100

    Fix formatting

--- api/src/include/mrna.h
+++ api/src/include/mrna.h
@@ -1,3 +1,3 @@
 #include "STONNEModel.h"
 #include "Config.h"
-void mRNA_tiles(Stonne *stonne_instance, Config stonne_cfg, int X, int Y, int C, int R, int S, int X_, int Y_, int K, int N, int stride);
+void mRNA_conv(Stonne *stonne_instance, Config stonne_cfg, int X, int Y, int C, int R, int S, int X_, int Y_, int K, int N, int stride);
